2f8a9fa1031b33c316ae2f3e5f8f41dcad498983
Jaeha Lee <wogkdkrm112@gmail.com>
2023-10-07 02:12:41 +0900

    feat: guessing number

---
+++ guessing_game/README.md
@@ -0,0 +1,4 @@
+## 프로젝트 생성
+
+- `cargo new guessing_game --bin`
+- `cargo run`
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/leejaeha/rust/Rust_basic/guessing_game/target/debug/guessing_game: /Users/leejaeha/rust/Rust_basic/guessing_game/src/main.rs

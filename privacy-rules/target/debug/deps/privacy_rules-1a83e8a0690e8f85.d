afe9b44d5848e9b73a7ca42b444e63038d4d4f84
Jaeha Lee <wogkdkrm112@gmail.com>
2023-10-08 22:20:10 +0900

    feat: privacy rules

---
+++ privacy-rules/target/debug/deps/privacy_rules-1a83e8a0690e8f85.d
@@ -0,0 +1,7 @@
+/Users/leejaeha/rust/learn-rust/privacy-rules/target/debug/deps/privacy_rules-1a83e8a0690e8f85.rmeta: src/lib.rs
+
+/Users/leejaeha/rust/learn-rust/privacy-rules/target/debug/deps/libprivacy_rules-1a83e8a0690e8f85.rlib: src/lib.rs
+
+/Users/leejaeha/rust/learn-rust/privacy-rules/target/debug/deps/privacy_rules-1a83e8a0690e8f85.d: src/lib.rs
+
+src/lib.rs:
---
+++ privacy-rules/target/debug/deps/privacy_rules-5b78cd2f0a678faa.d
@@ -0,0 +1,5 @@
+/Users/leejaeha/rust/learn-rust/privacy-rules/target/debug/deps/privacy_rules-5b78cd2f0a678faa.rmeta: src/lib.rs
+
+/Users/leejaeha/rust/learn-rust/privacy-rules/target/debug/deps/privacy_rules-5b78cd2f0a678faa.d: src/lib.rs
+
+src/lib.rs:

349bcf3eccba240b2e7d560060ab5369b4d041fa
Jaeha Lee <wogkdkrm112@gmail.com>
2023-10-08 22:22:17 +0900

    chore: remove target files

--- privacy-rules/target/debug/deps/privacy_rules-0bc868b17c68a607.d
+++
@@ -1,5 +0,0 @@
-/Users/leejaeha/rust/learn-rust/privacy-rules/target/debug/deps/privacy_rules-0bc868b17c68a607.rmeta: src/lib.rs
-
-/Users/leejaeha/rust/learn-rust/privacy-rules/target/debug/deps/privacy_rules-0bc868b17c68a607.d: src/lib.rs
-
-src/lib.rs:
--- privacy-rules/target/debug/deps/privacy_rules-5b78cd2f0a678faa.d
+++
@@ -1,5 +0,0 @@
-/Users/leejaeha/rust/learn-rust/privacy-rules/target/debug/deps/privacy_rules-5b78cd2f0a678faa.rmeta: src/lib.rs
-
-/Users/leejaeha/rust/learn-rust/privacy-rules/target/debug/deps/privacy_rules-5b78cd2f0a678faa.d: src/lib.rs
-
-src/lib.rs:

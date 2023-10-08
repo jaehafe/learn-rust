512bd1fc47af596f7508e00cc1d224cb93cc1631
Jaeha Lee <wogkdkrm112@gmail.com>
2023-10-08 22:57:58 +0900

    chore: remove target files

--- importing-names/target/debug/deps/importing_names-e1f6a41330b18082.d
+++
@@ -1,5 +0,0 @@
-/Users/leejaeha/rust/learn-rust/importing-names/target/debug/deps/importing_names-e1f6a41330b18082.rmeta: src/main.rs
-
-/Users/leejaeha/rust/learn-rust/importing-names/target/debug/deps/importing_names-e1f6a41330b18082.d: src/main.rs
-
-src/main.rs:
--- importing-names/target/debug/deps/importing_names-faf63062b01d546a.d
+++
@@ -1,5 +0,0 @@
-/Users/leejaeha/rust/learn-rust/importing-names/target/debug/deps/importing_names-faf63062b01d546a.rmeta: src/main.rs
-
-/Users/leejaeha/rust/learn-rust/importing-names/target/debug/deps/importing_names-faf63062b01d546a.d: src/main.rs
-
-src/main.rs:
